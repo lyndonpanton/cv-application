60177271a2736bc2fb102d967a1967a93323ac60
Lyndon Panton <LyndonPanton@gmail.com>
2025-11-24 17:29:44 +0000

    Progress (feature): Deleting key skill
    Users can delete a key skill from the preview of their key skills list

--- client/src/components/main/input/SkillsInput.jsx
+++ client/src/components/main/input/SkillsInput.jsx
@@ -1,10 +1,46 @@
 function SkillsInput(props) {
     // Use this to update props
-    const skills = [];
+    let skills = [];
     let currentId = 1;
 
     function deleteKeySkill(e) {
+        const id = e.target.parentElement["data-id"];
 
+        // Update skills list in data
+        skills = skills.filter(function (skill) {
+            return skill.id !== id;
+        });
+
+        // Update skills list in ui
+        const list = document.getElementById("key-skills-input-list");
+
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+
+        skills.forEach(function (skill) {
+            const listItem = document.createElement("li");
+            listItem.classList.add("key-skills-input-list-item");
+            listItem["data-id"] = skill.id;
+
+            const listItemTitle = document.createElement("span");
+            listItemTitle.classList.add("key-skills-input-list-item-title");
+            listItem.textContent = skill.title;
+
+            const listItemDelete = document.createElement("button");
+            listItemDelete.classList.add("key-skills-input-list-item-button");
+            // Attribute for changing assistive title. Change text content to "X"
+            // icon afterwards
+            listItemDelete.textContent = "Delete";
+            listItemDelete.type = "button";
+            listItemDelete.addEventListener("click", deleteKeySkill);
+            listItemDelete.addEventListener("keydown", deleteKeySkill);
+
+            listItem.appendChild(listItemTitle);
+            listItem.appendChild(listItemDelete);
+
+            list.appendChild(listItem);
+        });
     }
 
     function updateKeySkills(e) {
@@ -63,6 +99,8 @@ function SkillsInput(props) {
         // icon afterwards
         newListItemDelete.textContent = "Delete";
         newListItemDelete.type = "button";
+        newListItemDelete.addEventListener("click", deleteKeySkill);
+        newListItemDelete.addEventListener("keydown", deleteKeySkill);
 
         newListItem.appendChild(newListItemTitle);
         newListItem.appendChild(newListItemDelete);
